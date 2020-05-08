981bb46a933dfa51e5c0475da38a409e4e1d8dc1
mehr30na <mehr30na@gmail.com>
2020-05-08 20:27:20 +0200

    Delete Secrets.js

--- src/constants/Secrets.js
+++
@@ -1,3 +0,0 @@
-export const GOOGLE_PLACES_API_KEY = 'AIzaSyAmOWHv5PjYNaOQvShNSRKdBtdq4xMn3QA'
-export const GOOGLE_AUTOCOMPLETE_URL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${GOOGLE_PLACES_API_KEY}`
-export const GOOGLE_PLACES_DETAILS_URL = `https://maps.googleapis.com/maps/api/place/details/json?key=${GOOGLE_PLACES_API_KEY}&fields=geometry`
